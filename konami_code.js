98e823c1145d180f171b2ef23743f4b61f27ef3d
Uchenna Nwokorie <chen.nwokorie@gmail.com>
2018-10-17 23:47:59 +0000

    automatically backed up by learn

--- konami_code.js
+++ konami_code.js
@@ -1,3 +1,20 @@
  const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65]
 
-  
+let index = 0;
+ 
+// This is the function that would be invoked by the event listener.
+function init(e) {
+  const key = parseInt(e.detail || e.which);
+ 
+  if (key === alphabet[index]) {
+    index++;
+ 
+    if (index === alphabet.length) {
+      alert("Hurray!");
+ 
+      index = 0;
+    }
+  } else {
+    index = 0;
+  }
+}
