7a993214c52c5b484b11c69b9fa03d4163a4a89f
Uchenna Nwokorie <chen.nwokorie@gmail.com>
2018-10-18 00:02:55 +0000

    automatically backed up by learn

--- konami_code.js
+++ konami_code.js
@@ -4,7 +4,6 @@ let index = 0;
  
 // This is the function that would be invoked by the event listener.
 function init(e) {
-  console.log(e.which);
   const key = parseInt(e.detail || e.which);
  
   if (key === code[index]) {
